5627f165ab299f715bf14e2ad912a622899d8907
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2023-07-03 11:32:48 -0400

    Update select_compress_lineages.R
    Added import_monocle function and class definition

--- R/select_compress_lineages.R
+++ R/select_compress_lineages.R
@@ -1,1 +1,7 @@
+setClass("cell_data_set_ext", contains = "cell_data_set", slots=c(graphs = "list", lineages="list", expression="list", expectation="list", pseudotime="list")) -> cell_data_set_ext
 
+#' @export
+import_monocle <-function(cds){
+cds <- as(cds,"cell_data_set_ext")
+return(cds)
+}
