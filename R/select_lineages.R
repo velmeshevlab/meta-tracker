e417ede04e9e27a7d5bd482583fcaf4cadd78617
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2023-12-15 00:08:26 -0500

    Update select_lineages.R

--- R/select_lineages.R
+++ R/select_lineages.R
@@ -57,10 +57,10 @@ names1 = names(dd[dd > 2 | dd == 1])
 names2 = names(dd[dd == 2])
 names2 = sample(names2, length(names2)/N, replace = F)
 d.f = d[c(names1, names2),]
-ggplot(data=d, aes(x=umap_1, y=umap_2)) + geom_point(size=0.01) + geom_text_repel(data=d.f, aes(x=UMAP_1, y=UMAP_2), label=rownames(d.f), size=size, hjust = 2, color = "red", max.overlaps = Inf, segment.size = 0.1) + monocle_theme_opts()
+ggplot(data=d, aes(x=umap_1, y=umap_2)) + geom_point(size=0.01) + geom_text_repel(data=d.f, aes(x=umap_1, y=umap_2), label=rownames(d.f), size=size, hjust = 2, color = "red", max.overlaps = Inf, segment.size = 0.1) + monocle_theme_opts()
 }
 else{
-ggplot(data=d, aes(x=umap_1, y=umap_2)) + geom_point(size=0.01) + geom_text(data=d, aes(x=UMAP_1, y=UMAP_2), label=rownames(d), size=size, hjust = 1, color = "red") + monocle_theme_opts()
+ggplot(data=d, aes(x=umap_1, y=umap_2)) + geom_point(size=0.01) + geom_text(data=d, aes(x=umap_1, y=umap_2), label=rownames(d), size=size, hjust = 1, color = "red") + monocle_theme_opts()
 }
 }
 
