69f280a2ee639df44da616190ee0c0c48b112309
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2024-11-06 15:40:17 -0500

    Update between_lineage_DE.R

--- R/between_lineage_DE.R
+++ R/between_lineage_DE.R
@@ -1,6 +1,3 @@
-require(tradeSeq)
-require(dplyr)
-
 between_lineage_DE <- function(counts, # A matrix with genes in rows and cells in columns, cells should be aligned in the order of separate lineages
                                pseudotime, # A matrix of pseudotime values, each row represents a cell and each column represents a lineage, the order of lineages should correspond to the order of cells in "counts"
                                cellWeights, # A matrix of cell weights defining the probability that a cell belongs to a particular lineage.
