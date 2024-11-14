11ea7ea042fd82ff883a0d52d6ad55cd11a457c5
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2024-11-14 10:55:07 -0500

    Update differential_expression.R

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -10,7 +10,7 @@ within_lineage_DE <- function(cds, #metatracker object
                           ){
   counts = cds_new@expression[lineage] #a matrix of expression values, with genes in rows and cells in columns
   pseudotime = cds_new@pseudotime[lineage] #a matrix of pseudotime values, each row for a cell
-  cell_wt<-as.matrix(rep(1,ncol(expr_matrix)),ncol=1)
+  cell_wt<-as.matrix(rep(1,ncol(counts)),ncol=1)
   gamList<-tradeSeq::fitGAM(counts=counts,conditions=conditions,nknots=nknots,pseudotime=pseudotime,cellWeights=cell_wt)
   res<-tradeSeq::conditionTest(gamList,global=TRUE,pairwise=pairwise,lineages=FALSE)
   res_full<-res[!is.na(res$pvalue),]
