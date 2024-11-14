dadddb543dc65901f6cd1a923dba4214a7e8c2ce
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2024-11-14 10:46:45 -0500

    Update differential_expression.R

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -2,7 +2,7 @@
 library(tradeSeq)
 
 within_lineage_DE <- function(counts = expr_matrix,  #a matrix of expression values, with genes in rows and cells in columns
-                          conditions = condition_vector, #a vector of condition information
+                          conditions = NULL, #a vector of condition information
                           nknots = 3, #number of knots used to fit the GAM
                           pseudotime = pseudotime_matrix, #a matrix of pseudotime values, each row for a cell
                           pairwise=TRUE #pairwise comparison between different conditions
