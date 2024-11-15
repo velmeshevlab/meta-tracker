a7365bad107a809522082c505a202176a34d2766
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2024-11-15 11:31:39 -0500

    Update between_lineage_DE.R

--- R/between_lineage_DE.R
+++ R/between_lineage_DE.R
@@ -1,3 +1,29 @@
+calculate_weight_matrix <- function(cds)
+  {
+  lineages = names(cds@lineages)
+  all_cells = c()
+  lineage_list = list()
+  i = 1
+  for(lineage in lineages){
+    all_cells = c(all_cells, cds@lineages[[lineage]])
+    lineage_list[[i]] <- cds@lineages[[lineage]]
+    i <- i + 1
+  }
+  all_cells = unique(all_cells)
+  weight_matrix <- matrix(0, nrow = length(all_cells), ncol = length(lineages))
+  rownames(weight_matrix) <- all_cells
+  colnames(weight_matrix) <- names(cds@lineages)
+  for (i in seq_along(all_cells)) {
+    member <- all_cells[i]
+    # Check membership in each list
+    membership <- sapply(lineage_list, function(lst) member %in% lst)
+    # Determine the number of lists the member belongs to
+    num_lists <- sum(membership)
+    # Assign weights based on membership
+    weight_matrix[i, membership] <- 1 / num_lists
+  }
+  }
+
 between_lineage_DE <- function(counts, # A matrix with genes in rows and cells in columns, cells should be aligned in the order of separate lineages
                                pseudotime, # A matrix of pseudotime values, each row represents a cell and each column represents a lineage, the order of lineages should correspond to the order of cells in "counts"
                                cellWeights, # A matrix of cell weights defining the probability that a cell belongs to a particular lineage.
