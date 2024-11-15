e5f3de9c5fb9a966c04c2ccf75cb69d54d5e6024
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2024-11-15 14:39:24 -0500

    Update between_lineage_DE.R

--- R/between_lineage_DE.R
+++ R/between_lineage_DE.R
@@ -1,4 +1,4 @@
-calculate_weight_matrix <- function(cds)
+calculate_weight_matrix <- function(cds) #calculate metacell weights based on lineage membership. If a metacell belongs to more than one lineage, divide the weights between the lineages.
   {
   lineages = names(cds@lineages)
   all_cells = c()
