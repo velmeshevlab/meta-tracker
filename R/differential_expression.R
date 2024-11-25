4aa514431cd1dcba8fcfb4f95ff5d0825a78f4c0
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2024-11-25 15:24:30 -0500

    Update differential_expression.R

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -32,8 +32,8 @@ within_lineage_DE <- function(cds, #metatracker object
     }
   exp_95_max = rowMax(exp_95)
   names(exp_95_max) <- rownames(exp_95)
-  scaled_FC = res_sig$meanLogFC/exp_95_max
-  res_sig$scaled_FC <- scaled_FC                      
+  scaled_FC = res$meanLogFC/exp_95_max
+  res$scaled_FC <- scaled_FC                      
   res_sig = res[res$pvalue<p,]
   res_sig = res_sig[with(res_sig, order(pvalue, -meanLogFC)), ]
   cds@dynamic_genes[[lineage]] <- res_sig
