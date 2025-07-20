368c004ed82e135a34a113b859a254c020ab3be6
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2025-07-20 16:27:15 -0400

    Update select_lineages.R

--- R/select_lineages.R
+++ R/select_lineages.R
@@ -722,7 +722,7 @@ cds_subset@principal_graph[["UMAP"]] <- sub.graph
 cds_subset@principal_graph_aux[["UMAP"]]$dp_mst <- nodes_UMAP[,names(V(sub.graph))]
 cds_subset@clusters[["UMAP"]]$partitions <- cds_subset@clusters[["UMAP"]]$partitions[colnames(cds_subset)]
 #recalculate closest vertex for the selected cells
-cells_UMAP = as.data.frame(cds_subset@reducedDims[["UMAP"]])
+cells_UMAP = as.data.frame(reducedDims(cds_subset)[["UMAP"]])
 closest_vertex = apply(cells_UMAP[,c("UMAP_1", "UMAP_2")], 1, calculate_closest_vertex, nodes = as.matrix(nodes_UMAP[,names(V(sub.graph))]))
 closest_vertex = as.data.frame(closest_vertex)
 cds_subset@principal_graph_aux[["UMAP"]]$pr_graph_cell_proj_closest_vertex <- closest_vertex
