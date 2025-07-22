e05d4d59c9396c5601349dd9e52393210720c397
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2025-07-22 14:33:54 -0400

    Update differential_expression.R

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -18,6 +18,7 @@ within_lineage_DE <- function(lineage, #name of the lineage to analyze
                           FC = 0.5, #fold change value threshold
                           parallel = F
                           ){
+  print(paste0("Testing lineage ", lineage)
   d =cds@expression[[lineage]]
   #conditions = d[,conditions]
   counts = t(as.matrix(sapply(d[,4:ncol(d)], as.numeric))) #a matrix of expression values, with genes in rows and cells in columns
