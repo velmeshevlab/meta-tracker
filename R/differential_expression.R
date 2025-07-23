1284fdf5fe54cfbe748c68972295412f75f942e6
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2025-07-23 16:40:39 -0400

    Update differential_expression.R

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -21,9 +21,9 @@ within_lineage_DE <- function(lineage, #name of the lineage to analyze
                           ){
   print(paste0("Testing lineage ", lineage))
   d =cds@expression[[lineage]]
-  d = d[rownames(cds),]
   #conditions = d[,conditions]
   counts = t(as.matrix(sapply(d[,4:ncol(d)], as.numeric))) #a matrix of expression values, with genes in rows and cells in columns
+  counts = counts[rownames(cds),]
   pseudotime = as.matrix(cds@pseudotime[[lineage]]) #a matrix of pseudotime values, each row for a cell
   cell_wt<-as.matrix(rep(1,ncol(counts)),ncol=1)
   #gamList<-tradeSeq::fitGAM(counts=counts,conditions=conditions,nknots=nknots,pseudotime=pseudotime,cellWeights=cell_wt,sce=FALSE)
