e81175ba3ec302851598b4d5a467bcd38a94e5c4
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2025-07-22 23:21:50 -0400

    Update differential_expression.R

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -4,6 +4,7 @@
 within_lineage_DE_par <- function(cds, cores){
 lineages = names(cds@lineages)
 out <- mclapply(lineages, within_lineage_DE, cds = cds, mc.cores = cores)
+names(out) <- lineages
 cds@dynamic_genes <- out
 cds
 }
