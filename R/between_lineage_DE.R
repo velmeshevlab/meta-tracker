73042d61066ebfab7ac183c40bb07c513984b297
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2025-07-28 02:37:24 -0400

    Update between_lineage_DE.R

--- R/between_lineage_DE.R
+++ R/between_lineage_DE.R
@@ -194,7 +194,7 @@ get_lineage_genes <- function(cds, test_lineage, genes = NULL, U = NULL, nknots 
     else{
       p_values_sel = as.matrix(p_values)
       }
-    if(nrow(p_values_sel) == 0{
+    if(nrow(p_values_sel) == 0){
       NULL
     }
     gene_names = rownames(p_values_sel)
