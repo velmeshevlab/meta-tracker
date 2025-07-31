55b22367bd3d0a1c9d82c10d31c1fef5ba5cb83c
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2025-07-30 22:59:29 -0400

    Update differential_expression.R

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -46,7 +46,7 @@ within_lineage_DE <- function(lineage, #name of the lineage to analyze
     exp_95 = cbind(exp_95, exp_95_lin)
     }
   rownames(exp_95) <- rownames(res)
-  exp_95_max = rowMax(exp_95)
+  exp_95_max = apply(exp_95, 1, max, na.rm = TRUE)
   expectation = cds@expectation[[lineage]]
   expectation = expectation[,rownames(cds)]
   FC_factor = apply(expectation, 2, function(x) as.numeric(quantile(x, 0.95, na.rm= TRUE)))/exp_95_max
