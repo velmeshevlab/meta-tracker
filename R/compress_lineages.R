4f5755b7134375f70c6c2d16dd90c79807987b5a
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2026-01-27 13:32:01 -0500

    Update compress_lineages.R

--- R/compress_lineages.R
+++ R/compress_lineages.R
@@ -8,15 +8,15 @@ filter_by_expression <- function(cds, mode = "number", N = 100, ratio = 0.01){
   data = counts(cds)
   lineages = names(cds@lineages)
   all.expressed_genes = c()
-  if(mode == "ratio"){
-   cutoff = ratio*ncol(data.sub)
-  }
-  else{
-   cutoff = N
-  }
   for(lineage in lineages){
     cells = as.character(cds@lineages[[lineage]])
     data.sub = data[,cells]
+    if(mode == "ratio"){
+     cutoff = ratio*ncol(data.sub)
+    }
+    else{
+     cutoff = N
+    }
     expressed_genes = rownames(data.sub)[rowSums(data.sub> 0) > cutoff]
     all.expressed_genes = append(all.expressed_genes, expressed_genes)
   }
